dd90afb70588ac53e498d960eff1077450fc06fd
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2023-12-18 17:51:54 +0530

    Implemented an example for view model

---
+++ counter/app/src/main/java/com/example/counter/MyViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.counter
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class MyViewModel(): ViewModel() {
+    var count by mutableStateOf(0)
+
+    fun increaseCounter() {
+        count++
+    }
+}
